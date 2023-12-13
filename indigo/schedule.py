fd746deb50c30f2fa127a11e0fb593cc2ccb7db0
Luis Tripa <luistripa@me.com>
2023-12-13 14:10:22 +0000

    Improved schedule and actionGroup docstrings

--- indigo/schedule.py
+++ indigo/schedule.py
@@ -8,40 +8,40 @@ def create(*args):
     ...
 
 def delete(*args):
-    """There does seem to be any documentation for this method in the API docs."""
+    """There doesn't seem to be any documentation for this method in the API docs."""
     ...
 
 
 def displayInRemoteUI(*args):
-    """There does seem to be any documentation for this method in the API docs."""
+    """There doesn't seem to be any documentation for this method in the API docs."""
     ...
 
 
 def duplicate(*args):
-    """There does seem to be any documentation for this method in the API docs."""
+    """There doesn't seem to be any documentation for this method in the API docs."""
     ...
 
 
 def enable(*args):
-    """There does seem to be any documentation for this method in the API docs."""
+    """There doesn't seem to be any documentation for this method in the API docs."""
     ...
 
 
 def execute(*args):
-    """There does seem to be any documentation for this method in the API docs."""
+    """There doesn't seem to be any documentation for this method in the API docs."""
     ...
 
 
 def getDependencies(*args):
-    """There does seem to be any documentation for this method in the API docs."""
+    """There doesn't seem to be any documentation for this method in the API docs."""
     ...
 
 
 def moveToFolder(*args):
-    """There does seem to be any documentation for this method in the API docs."""
+    """There doesn't seem to be any documentation for this method in the API docs."""
     ...
 
 
 def removeDelayedActions(*args):
-    """There does seem to be any documentation for this method in the API docs."""
+    """There doesn't seem to be any documentation for this method in the API docs."""
     ...
